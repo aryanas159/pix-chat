f9d677020348c585bfd29febe4b98eda23ef0294
Aryan Singh <ssaryans597@gmail.com>
2023-11-25 21:18:36 +0530

    cors issue fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
 	cors: {
-		origin: process.env.ORIGIN_URL,
+		origin: "https://pix-next.vercel.app",
 	},
 });
 const PORT = process.env.PORT || 4000;
